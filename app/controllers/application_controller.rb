5be73be4757e2548ce9d6f2d0caba88ce6f98c07
Thang Nguyen <thangnguyen@Thangs-MacBook-Pro.local>
2017-11-11 17:19:32 +0700

    update dependencies in circle.yml and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, World! I am Thang"
+  end
+
+  def goodbye
+    render html: "Goodbye, I will be back soon"
+  end
 end
